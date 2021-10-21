9dc3bbe68f586b62ac62dc96baa6812829a065cd
Patrickrobsg <patrickrobsg@hotmail.com>
2021-10-21 11:51:47 -0300

    Versão inicial do codigo em react native do projeto

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>JSHDAJKSGKJASHDK</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { View, TextInput, Button } from 'react-native';
+import { withFormik } from 'formik';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    }
+  }
+)
+
+const Form = (props) => (
+  <View style={styles.container}>
+    <TextInput
+      value={props.values.email}
+      onChangeText={text => props.setFieldValue('email', text)}
+    />
+    <TextInput
+      value={props.values.password}
+      onChangeText={text => props.setFieldValue('password', text)}
+    />
+    <Button
+      onPress={props.handleSubmit}
+      title="Login"
+    />
+  </View>
+);
+
+export default withFormik({
+  mapPropsToValues: () => ({ email: '', password: '' }),
+  handleSubmit: (values) => {
+    console.log(values);
+  }
+})(Form);
