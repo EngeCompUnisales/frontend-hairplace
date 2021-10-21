c183f5b05f7926e76e1b6c1dfa399d51d8c39575
Patrick Robert da Silva Gomes <patrick@gvp.local>
2021-10-21 16:21:21 -0300

    Atualização do 'Style' do front

--- App.js
+++ App.js
@@ -2,32 +2,47 @@ import React from 'react';
 import { StyleSheet, View, TextInput, Button } from 'react-native';
 import { withFormik } from 'formik';
 
-const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    }
-  }
-)
-
 const Form = (props) => (
   <View style={styles.container}>
     <TextInput
+      style={styles.input}
+      placeholder="E-Mail"
       value={props.values.email}
       onChangeText={text => props.setFieldValue('email', text)}
     />
     <TextInput
+      secureTextEntry={true}
+      style={styles.password}
+      placeholder="Senha"
       value={props.values.password}
       onChangeText={text => props.setFieldValue('password', text)}
     />
     <Button
       onPress={props.handleSubmit}
-      title="Login"
+      title="Entrar"
     />
   </View>
 );
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  password: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    autoComplete: 'password',
+  }
+});
 
 export default withFormik({
   mapPropsToValues: () => ({ email: '', password: '' }),
