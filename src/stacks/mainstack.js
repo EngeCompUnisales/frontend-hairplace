5252c97df75ad98d647b882058ba77b1ff8aa9c3
Jonathan Trabach <52935711+Jonathan-trabach@users.noreply.github.com>
2021-10-25 21:07:59 -0300

    refazendo 2

---
+++ src/stacks/mainstack.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import preload from "../pages/Preload";
+import signin from "../pages/SignIn";
+import signup from "../pages/SignUp";
+
+const Stack = createStackNavigator();
+
+export default () => (
+    <Stack.Navigator
+        initialRouteName="preload"
+        screenOptions={{
+            headerShown: false
+        }}
+    >
+        <Stack.Screen name="Preload" component={preload} />
+        <Stack.Screen name="SignIn" component={signin} />
+        <Stack.Screen name="SignUp" component={signup} />
+    </Stack.Navigator>
+);
